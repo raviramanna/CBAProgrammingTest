30ab3d629c9cd46f2da344ebbaa2dbe51e802edc
RaviRamanna - Ravi Ramanna <raviramanna@hotmail.com.au>
2018-03-21 16:20:08 +1100

    Added some test cases to verify the webapi is working

---
+++ TeleTextWebApiServer/TeleTextWebApiServer.Tests/Controllers/TeleTextControllerTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TeleTextWebApiServer;
+using TeleTextWebApiServer.Controllers;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TeleTextWebApiServer.Tests.Controllers
+{
+    [TestClass]
+    public class TeletextControllerTest
+    {
+        [TestMethod]
+        public void GetTeleText()
+        {
+            // Set up Prerequisites   
+            var controller = new TeleTextController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+            var id = 1;
+            // Act on Test  
+            var response = controller.Get();
+            // Assert the result  
+            foreach (var line in response)
+            {
+                Assert.AreEqual("TeleText"+id, line);
+                id++;
+            }
+        }
+
+        [TestMethod]
+        public void GetTeleTextWithId()
+        {
+            // Set up Prerequisites   
+            var controller = new TeleTextController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+            var id = 1;
+            // Act on Test  
+            var response = controller.Get(id);
+            // Assert the result  
+            
+            Assert.AreEqual("TeleText" + id, response);
+        }
+    }
+}
---
+++ TeleTextWebApiServer/TeleTextWebApiServer/Controllers/TeleTextController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TeleTextWebApiServer.Controllers
+{
+    public class TeleTextController : ApiController
+    {
+        // GET: api/TeleText
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "TeleText1", "TeleText2" };
+        }
+
+        // GET: api/TeleText/5
+        public string Get(int id)
+        {
+            return "TeleText" + id;
+        }
+
+        // POST: api/TeleText
+        public void Post([FromBody]string value)
+        {
+
+        }
+        
+    }
+}
