aa6eb6316981b4aad62cbb601089d292cc8f3ac9
RaviRamanna - Ravi Ramanna <raviramanna@hotmail.com.au>
2018-05-10 18:25:13 +1000

    Called the API using webclient and tested working fine
    The client is able to call the Web API using Webclient and is tested. The result data has the formatted text.

---
+++ TeleTextWebApiServer/TeleTextWebApiClient/Models/APIDTOs/SuccessDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeleTextWebApiServer.Models
+{
+    public class SuccessDTO
+    {
+        public int success { get; set; }
+        public string[] message { get; set; }
+    }
+}
